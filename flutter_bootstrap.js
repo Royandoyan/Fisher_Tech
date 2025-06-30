d601788e9f43129cf98168bbf26a89d5d59b7ad2
Royandoyan <Royandoyan@users.noreply.github.com>
2025-06-30 05:17:52 +0000

    deploy: e33b7af5eeebddcb51d2d83be6678b9c039c9088

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4074565280"
+    serviceWorkerVersion: "1303557325"
   }
 });
